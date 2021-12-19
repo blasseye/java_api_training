8bdcd7ebbb08056042aafc01f05b109ab8961327
blasseye <lasseye@et.esiea.fr>
2021-12-19 17:27:27 +0100

    MAJ Launcher

--- src/main/java/fr/lernejo/navy_battle/Launcher.java
+++ src/main/java/fr/lernejo/navy_battle/Launcher.java
@@ -1,2 +1,23 @@
-package fr.lernejo.navy_battle;public class Launcher {
+package fr.lernejo.navy_battle;
+
+import fr.lernejo.navy_battle.server.Http_Server;
+
+public class Launcher {
+
+    public static void main(String[] args) throws Exception {
+        if (args.length == 1) {
+            int port = Integer.parseInt(args[0]);
+            new Http_Server(port);
+        } else if ( args.length == 2) {
+            int port = Integer.parseInt(args[0]);
+            String targetURL = args[1];
+            /*
+            NavyClient myNavyClient = new NavyClient(port, targetURL);
+            myNavyClient.play();
+            */
+        } else {
+            System.out.println("Argument manquant.");
+        }
+    }
 }
+
