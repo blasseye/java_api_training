313d541d001566d2dfad477e14a1ebd71a219b48
blasseye <lasseye@et.esiea.fr>
2021-12-30 17:53:39 +0100

    modification RESPONSE start dans LAUNCHER
    modification RESPONSE start dans LAUNCHER v2

--- src/main/java/fr/lernejo/navy_battle/Launcher.java
+++ src/main/java/fr/lernejo/navy_battle/Launcher.java
@@ -26,7 +26,7 @@ public class Launcher {
     }
     static class JsonStartHandler implements HttpHandler {
 
-        private final String RESPONSE = "\"{\\\"id\\\":\\\"1\\\", \\\"url\\\":\\\"http://localhost:\" + port + \"\\\", \\\"message\\\":\\\"hello\\\"}\"";
+        private final String RESPONSE = "{\"id\":\"1\", \"url\":\"http://localhost:8080\")";
 
         public void handle(HttpExchange t) throws IOException {
             t.sendResponseHeaders(200, RESPONSE.length());
